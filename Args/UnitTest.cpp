55cf426f4a7b51362909121d1a6b4e111a71f695
rxu18 <rxu18@RXU18-MOBL1>
2016-12-21 20:32:01 +0800

    add unit test for int and string

--- Args/UnitTest.cpp
+++ Args/UnitTest.cpp
@@ -22,10 +22,26 @@ TEST(test_boolean_args, schema_with_leading_tailing_space)
 TEST(test_boolean_args, with_multiple_flags_and_spaces)
 {
     string schema("  l   ,m , n, o ");
-    vector<string> args{ "-l -n" };
+    vector<string> args{ "-l","-n" };
     Args arg(schema, args);
     EXPECT_EQ(true, arg.getBoolean('l'));
     EXPECT_EQ(false, arg.getBoolean('m'));
     EXPECT_EQ(true, arg.getBoolean('n'));
     EXPECT_EQ(false, arg.getBoolean('o'));
+}
+
+TEST(test_int_args, with_one)
+{
+    string schema("p#");
+    vector<string> args{ "-p", "1234" };
+    Args arg(schema, args);
+    EXPECT_EQ(1234, arg.getInt('p'));
+}
+
+TEST(test_string_args, with_one)
+{
+    string schema("d*");
+    vector<string> args{ "-d", "www.intel.com" };
+    Args arg(schema, args);
+    EXPECT_EQ(string("www.intel.com"), arg.getString('d'));
 }
