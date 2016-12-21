7742fa045eaf8ef92588cbce621eb09e367b73e8
rxu18 <rxu18@RXU18-MOBL1>
2016-12-21 16:22:20 +0800

    move methods from header to source file

--- Args/Args.cpp
+++ Args/Args.cpp
@@ -11,6 +11,8 @@ Args::Args(string schema, vector<string> args) {
     valid = parse();
 }
 
+Args::~Args(){}
+
 bool Args::parse() {
     if (schema.length() == 0 && args.size() == 0) {
         return true;
@@ -65,6 +67,24 @@ void Args::validateSchemaElementId(char elementId) {
             "Bad char: -------- in Args format: ");
     }
 }
+void Args::parseBooleanSchemaElement(char elementId) {
+    booleanArgs[elementId] = false;
+}
+void Args::parseIntegerSchemaElement(char elementId) {
+    intArgs[elementId] = 0;
+}
+void Args::parseStringSchemaElement(char elementId) {
+    stringArgs[elementId] = "";
+}
+bool Args::isStringSchemaElement(string elementTail) {
+    return elementTail == "*";
+}
+bool Args::isBooleanSchemaElement(string elementTail) {
+    return elementTail.length() == 0;
+}
+bool Args::isIntegerSchemaElement(string elementTail) {
+    return elementTail == "#";
+}
 
 bool Args::parseArguments() {
     for (currentArgument = 0; static_cast<size_t>(currentArgument) < args.size(); currentArgument++)
@@ -106,6 +126,10 @@ bool Args::setArgument(char argChar) {
     return true;
 }
 
+bool Args::isIntArg(char argChar) {
+    return intArgs.find(argChar) != intArgs.end();
+}
+
 void Args::setIntArg(char argChar) {
     currentArgument++;
     string parameter = "";
@@ -141,4 +165,74 @@ void Args::setStringArg(char argChar) {
     }
 }
 
+bool Args::isStringArg(char argChar) {
+    return stringArgs.find(argChar) != stringArgs.end();
+}
+void Args::setBooleanArg(char argChar, bool value) {
+    booleanArgs[argChar] = value;
+}
+bool Args::isBooleanArg(char argChar) {
+    return booleanArgs.find(argChar) != booleanArgs.end();
+}
+int Args::cardinality() {
+    return argsFound.size();
+}
+string Args::usage() {
+    if (schema.length() > 0)
+        return "-[" + schema + "]";
+    else
+        return "";
+}
+string Args::errorMessage() {
+    switch (errorCode) {
+    case OK:
+        throw new string("TILT: Should not get here.");
+    case UNEXPECTED_ARGUMENT:
+        return unexpectedArgumentMessage();
+    case MISSING_STRING:
+        return string("Could not find string parameter for");
+        /*
+        return string.format("Could not find string parameter for -%c.",
+        errorArgumentId);
+        */
+    case INVALID_INTEGER:
+        return string("Argument -%c expects an integer but was");
+        /*
+        return string.format("Argument -%c expects an integer but was '%s'.",
+        errorArgumentId, errorParameter);
+        */
+    case MISSING_INTEGER:
+        return string("Could not find integer parameter for");
+    }
+    return "";
+}
+string Args::unexpectedArgumentMessage() {
+    /*
+    StringBuffer message = new StringBuffer("Argument(s) -");
+    for (char c : unexpectedArguments) {
+    message.append(c);
+    }
+    message.append(" unexpected.");
+    return message.toString();
+    */
+    return "unexpectedArgumentMessage";
+}
+string Args::getString(char arg) {
+    auto iter = stringArgs.find(arg);
+    return iter == stringArgs.end() ? string("") : iter->second;
+}
+int Args::getInt(char arg) {
+    auto iter = intArgs.find(arg);
+    return iter == intArgs.end() ? 0 : iter->second;
+}
+bool Args::getBoolean(char arg) {
+    auto iter = booleanArgs.find(arg);
+    return iter == booleanArgs.end() ? false : iter->second;
+}
+bool Args::has(char arg) {
+    return argsFound.find(arg) != argsFound.end();
+}
+bool Args::isValid() {
+    return valid;
+}
 
--- Args/Args.h
+++ Args/Args.h
@@ -8,12 +8,55 @@ class ArgsException {
 };
 
 class Args {
-private:
+public:
+    Args(string schema, vector<string> args);
+    ~Args();
 
+    bool parse();
+    bool parseSchema();
+    void parseSchemaElement(string element);
+    void validateSchemaElementId(char elementId);
+    
+    void parseBooleanSchemaElement(char elementId);
+    void parseIntegerSchemaElement(char elementId);
+    void parseStringSchemaElement(char elementId);
+
+    bool isStringSchemaElement(string elementTail);
+    bool isBooleanSchemaElement(string elementTail);
+    bool isIntegerSchemaElement(string elementTail);
+
+    bool parseArguments();
+    void parseArgument(string arg);
+    void parseElements(string arg);
+    void parseElement(char argChar);
+
+    bool setArgument(char argChar);
+
+    bool isIntArg(char argChar);
+    void setIntArg(char argChar);
+
+    void setStringArg(char argChar);
+    bool isStringArg(char argChar);
+
+    void setBooleanArg(char argChar, bool value);
+    bool isBooleanArg(char argChar);
+
+    int cardinality();
+    string usage();
+    string errorMessage();
+    string unexpectedArgumentMessage();
+
+    string getString(char arg);
+    int getInt(char arg);
+    bool getBoolean(char arg);
+    bool has(char arg);
+    bool isValid();
+
+private:
     enum ErrorCode {
         OK, MISSING_STRING, MISSING_INTEGER, INVALID_INTEGER, UNEXPECTED_ARGUMENT
     };
-private:
+
     string schema;
     vector<string> args;
     bool valid = true;
@@ -26,111 +69,5 @@ private:
     char errorArgumentId = '\0';
     string errorParameter = "TILT";
     ErrorCode errorCode = ErrorCode::OK;
-public:
-    Args(string schema, vector<string> args);
-    bool parse();
-    bool parseSchema();
-    void parseSchemaElement(string element);
 
-    void validateSchemaElementId(char elementId);
-
-    void parseBooleanSchemaElement(char elementId) {
-        booleanArgs[elementId] = false;
-    }
-    void parseIntegerSchemaElement(char elementId) {
-        intArgs[elementId] = 0;
-    }
-    void parseStringSchemaElement(char elementId) {
-        stringArgs[elementId] = "";
-    }
-    bool isStringSchemaElement(string elementTail) {
-        return elementTail == "*";
-    }
-    bool isBooleanSchemaElement(string elementTail) {
-        return elementTail.length() == 0;
-    }
-    bool isIntegerSchemaElement(string elementTail) {
-        return elementTail == "#";
-    }
-    bool parseArguments();
-    void parseArgument(string arg);
-    void parseElements(string arg);
-    void parseElement(char argChar);
-
-    bool setArgument(char argChar);
-    bool isIntArg(char argChar) {
-        return intArgs.find(argChar) != intArgs.end();
-    }
-    void setIntArg(char argChar);
-    void setStringArg(char argChar);
-    bool isStringArg(char argChar) {
-        return stringArgs.find(argChar) != stringArgs.end();
-    }
-    void setBooleanArg(char argChar, bool value) {
-        booleanArgs[argChar] = value;
-    }
-    bool isBooleanArg(char argChar) {
-        return booleanArgs.find(argChar) != booleanArgs.end();
-    }
-    int cardinality() {
-        return argsFound.size();
-    }
-    string usage() {
-        if (schema.length() > 0)
-            return "-[" + schema + "]";
-        else
-            return "";
-    }
-    string errorMessage(){
-        switch (errorCode) {
-        case OK:
-            throw new string("TILT: Should not get here.");
-        case UNEXPECTED_ARGUMENT:
-            return unexpectedArgumentMessage();
-        case MISSING_STRING:
-            return string("Could not find string parameter for");
-            /*
-            return string.format("Could not find string parameter for -%c.",
-                errorArgumentId);
-                */
-        case INVALID_INTEGER:
-            return string("Argument -%c expects an integer but was");
-            /*
-            return string.format("Argument -%c expects an integer but was '%s'.",
-                errorArgumentId, errorParameter);
-                */
-        case MISSING_INTEGER:
-            return string("Could not find integer parameter for");
-        }
-        return "";
-    }
-    string unexpectedArgumentMessage() {
-        /*
-        StringBuffer message = new StringBuffer("Argument(s) -");
-        for (char c : unexpectedArguments) {
-            message.append(c);
-        }
-        message.append(" unexpected.");
-        return message.toString();
-        */
-        return "unexpectedArgumentMessage";
-    }
-    string getString(char arg) {
-        auto iter = stringArgs.find(arg);
-        return iter == stringArgs.end() ? string("") : iter->second;
-    }
-    int getInt(char arg) {
-        auto iter = intArgs.find(arg);
-        return iter == intArgs.end() ? 0 : iter->second;
-    }
-    bool getBoolean(char arg) {
-        auto iter = booleanArgs.find(arg);
-        return iter == booleanArgs.end() ? false : iter->second;
-    }
-    bool has(char arg) {
-        return argsFound.find(arg) != argsFound.end();
-    }
-    bool isValid() {
-        return valid;
-    }
 };
